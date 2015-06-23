e5d2ed9e5fe2e8a20158cafc64806b864af3b68f
Konstantin Heinrich <kheinrich188@live.de>
2015-06-23 11:26:13 +0200

    Add Core.js
    Simulate some model representation for object „Key“

---
+++ Core.js
@@ -0,0 +1,54 @@
+// Core.js
+// Created by Konstantin Heinrich am 23.06.2015
+//
+
+/*
+ History:
+ 
+ */
+
+
+function Point(x,y){
+    var self = this;
+    self.x = x
+    self.y = y;
+    return self
+};
+
+function Rect(x,y,widht,height) {
+    var self = this;
+    self.x = x;
+    self.y = y;
+    self.widht = widht;
+    self.height = height;
+    
+    return self;
+};
+
+function RectMake(x,y,widht, height) {
+    return Rect(x,y,widht,height);
+}
+
+
+function Key(frame){
+    
+    var self = this;
+    self.frame = frame;
+    self.value;
+    
+    self.setValue = function(text) {
+        self.value = text;
+    }
+    
+};
+
+var newKey = new Key(RectMake(0,0,20,20));
+alert(newKey.frame.widht);
+newKey.setValue("Hi");
+
+alert(newKey.value);
+
+
+
+
+
